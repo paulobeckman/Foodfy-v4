d5b20b8e10028f1ce0d2ae6b9f1def21eca0ac50
paulobeckman <beckmam.paulo@hotmail.com>
2020-07-28 17:09:45 -0300

    added and in progress show chefs recipes lists

--- src/app/models/Chef.js
+++ src/app/models/Chef.js
@@ -34,9 +34,10 @@ module.exports = {
     },
     find(id, callback) {
         db.query (`
-            SELECT * 
-            FROM chefs
-            WHERE id = $1`, [id], function (err, results) {
+        SELECT chefs.*, recipes.title AS recipes_title, recipes.image AS recipes_image
+        FROM chefs
+        LEFT JOIN recipes ON (chefs.id = recipes.chef_id)
+        WHERE chefs.id = $1`, [id], function (err, results) {
                 if(err) throw `Database error ${err}`
 
                 callback(results.rows[0])
--- src/app/models/Recipe.js
+++ src/app/models/Recipe.js
@@ -4,8 +4,9 @@ const { date } = require('../../lib/utils')
 module.exports = {
     all(callback) {
         db.query(`
-            SELECT * 
+            SELECT recipes.*, chefs.name AS chef_name 
             FROM recipes 
+            LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
             ORDER BY title ASC`, function (err, results) {
                 if(err) throw `Database error ${err}`
 
